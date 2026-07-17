bdb499b2fdff40e2cd24f9d584ccb76cb3647c95
Sobte <i@sobte.me>
2026-07-18 03:17:16 +0800

    migrate: snowfall-lib → purr, pre-commit-hooks → git-hooks
    - Replace snowfall-lib with purr flake framework
    - Rename pre-commit-hooks input to git-hooks
    - Rename checks/pre-commit-check to checks/git-hooks
    - Move checks/shells into develop/ directory
    - Replace channelsConfig with nixpkgsConfig
    - Update module namespace references (snowfallorg → ${namespace})

--- shells/default/default.nix
+++ shells/default/default.nix
@@ -1,19 +1,17 @@
 {
   inputs,
   pkgs,
-  mkShell,
   system,
   ...
 }:
-mkShell {
+pkgs.mkShell {
   packages = with pkgs; [
-    # nix stuff
     nixfmt
     deadnix
     statix
     inputs.colmena.packages.${system}.colmena
   ];
 
-  inherit (inputs.self.checks.${system}.pre-commit-check) shellHook;
-  buildInputs = inputs.self.checks.${system}.pre-commit-check.enabledPackages;
+  inherit (inputs.self.checks.${system}.git-hooks) shellHook;
+  buildInputs = inputs.self.checks.${system}.git-hooks.enabledPackages;
 }
